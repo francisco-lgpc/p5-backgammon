7dfe9bfe28bd173b739b8aaa540daa0b42253692
Francisco P Coutinho <francisco.lgpc@gmail.com>
2019-11-09 17:46:10 +0100

    added globals file

---
+++ globals.js
@@ -0,0 +1,16 @@
+const WHITE = 1
+const BLACK = 0
+
+const STATES = {
+  BEARED_OFF: 0,
+  BEARING_OFF: 1,
+  OUT_OPP_BOARD: 2,
+  IN_OPP_BOARD: 3
+}
+
+const DICE_SIZE = 75
+
+const BEAR_OFF_INDEX = {
+  WHITE: 0,
+  BLACK: 25
+}
